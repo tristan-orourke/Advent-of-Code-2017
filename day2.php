d671556cc8e65866917cf90d07958e08276d19fa
Unknown <tristan.m.orourke@gmail.com>
2017-12-12 21:01:50 -0500

    Created day2 challenge stub files

---
+++ day2.php
@@ -0,0 +1,16 @@
+<head>
+    <title>AoC: Day 2</title>
+    <meta name="author" content="Tristan O'Rourke" />
+    <meta name="description" content="Advent of Code 2017 Day 2 Solution" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <script type="text/javascript" src="./js/InverseCaptchaAPI.js"></script>
+</head>
+
+<body>
+    <?php
+        require("header.php");
+    ?>
+    <h2>Day 2: Corruption Checksum</h2>
+    <div class="puzzle" id="puzzle-1">
+    </div>
+</body>
