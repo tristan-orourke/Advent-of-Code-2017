009d09d46cb9c26899a483560eafa74e00c98c1d
Unknown <tristan.m.orourke@gmail.com>
2017-12-16 23:30:54 -0500

    Day 7 stub classes

---
+++ day7.php
@@ -0,0 +1,32 @@
+<head>
+    <title>AoC: Day 6</title>
+    <meta name="author" content="Tristan O'Rourke" />
+    <meta name="description" content="Advent of Code 2017 Day 7 Solution" />
+    <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+    <script type="text/javascript" src="./js/BalanceTreeAPI.js"></script>
+</head>
+
+<body>
+    <?php
+        require("header.php");
+    ?>
+    <h2>Day 7: Recursive Circus</h2>
+    <?php
+        require_once("puzzleSolverTemplates.php");
+        echo_puzzle_form('puzzle-1',
+                         'Puzzle 1: What is the root node?', 
+                         'form-1',
+                         'input-1',
+                         'node names, weights, and children',
+                         'solution-1');
+        echo_js_tying_form_to_function('input-1', 'form-1', 'solution-1', 'BalanceTreeAPI.findRootNode');
+        
+        echo_puzzle_form('puzzle-2',
+                         'Puzzle 2: How many memory allocation cycles within the loop?', 
+                         'form-2',
+                         'input-2',
+                         'memory blocks',
+                         'solution-2');
+        echo_js_tying_form_to_function('input-2', 'form-2', 'solution-2', 'BalanceTreeAPI.findRootNode');      
+    ?>
+</body>
