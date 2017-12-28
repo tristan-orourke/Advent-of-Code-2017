352deeb23e1dec5418524d753dbd3148b4153728
Unknown <tristan.m.orourke@gmail.com>
2017-12-28 13:33:36 -0500

    puzzle 2 solved in python

--- py/VirusGridAPI.py
+++ py/VirusGridAPI.py
@@ -3,6 +3,8 @@ class Carrier:
 	position = (0,0)
 	infections = 0
 	cleans = 0
+	weakens = 0
+	flags = 0
 
 	allDirections = [(0,1), (1,0), (0,-1), (-1, 0)]
 	directionNames = ['up','right','down', 'left']
@@ -15,6 +17,12 @@ class Carrier:
 		current = self.allDirections.index(self.direction)
 		left = (current - 1) % len(self.allDirections)
 		self.direction = self.allDirections[left]
+
+	def reverse(self):
+		numDirections = len(self.allDirections)
+		current = self.allDirections.index(self.direction)
+		reverse = (current + (numDirections//2)) % numDirections
+		self.direction = self.allDirections[reverse]
 
 	def moveForward(self):
 		self.position = (self.position[0]+self.direction[0], self.position[1]+self.direction[1])
@@ -36,6 +44,28 @@ class Carrier:
 			if debug:
 				print('infect')
 				self.printInfo()
+
+	def doBurstEvolved(self, infected, weakened, flagged):
+		if self.position in infected:
+			self.rotR()
+			infected.remove(self.position)
+			flagged.add(self.position)
+			self.flags += 1
+		elif self.position in weakened:
+			#continue same direction
+			weakened.remove(self.position)
+			infected.add(self.position)
+			self.infections += 1
+		elif self.position in flagged:
+			self.reverse()
+			flagged.remove(self.position)
+			self.cleans += 1
+		else:
+			#this position is clean
+			self.rotL()
+			weakened.add(self.position)
+			self.weakens += 1
+		self.moveForward()
 
 	def printInfo(self):
 		facing = self.directionNames[self.allDirections.index(self.direction)]
@@ -65,6 +95,20 @@ def run(initialGridString, bursts, debug=False):
 		carrier.doBurst(infectedNodes, debug=debug)
 	print(carrier.infections)
 
+def runEvolved(initialGridString, bursts, debug=False):
+	infected = initializeInfectedSet(initialGridString)
+	weakened = set()
+	flagged = set()
+	carrier = Carrier()
+	if debug:
+		print(infectedNodes)
+		carrier.printInfo()
+	for i in range(bursts):
+		carrier.doBurstEvolved(infected, weakened, flagged)
+		if debug:
+			carrier.printInfo()
+	print(carrier.infections)
+
 if __name__ == '__main__':
 	testGrid = '..#\n#..\n...'
 	print('Test grid 70 bursts:')
@@ -76,4 +120,9 @@ if __name__ == '__main__':
 	print('Real input 10000 bursts:')
 	run(realInput, 10000)
 
-
+	print('Test grid evolved 100 bursts:')
+	runEvolved(testGrid, 100)
+	print('Test grid evolved 10000000 bursts:')
+	runEvolved(testGrid, 10000000)
+	print('Real input evolved 10000000 bursts:')
+	runEvolved(realInput, 10000000)
