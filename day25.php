4a723650eb42d5257eac3d862bd8d355751a974f
tristan-orourke <tristan.m.orourke@gmail.com>
2017-12-29 16:34:09 -0500

    Day 25 complete

--- day25.php
+++ day25.php
@@ -20,13 +20,5 @@
                          'blueprints',
                          'solution-1');
         echo_js_tying_form_to_function('input-1', 'form-1', 'solution-1', 'TuringMachineAPI.getDiagnosticChecksumForStateMachine');
-        
-        echo_puzzle_form('puzzle-2',
-                         'Puzzle 2: ', 
-                         'form-2',
-                         'input-2',
-                         'blueprints',
-                         'solution-2');
-        echo_js_tying_form_to_function('input-2', 'form-2', 'solution-2', 'TuringMachineAPI.getDiagnosticChecksumForStateMachine');      
     ?>
 </body>
